53cda683b008734ca459e0e1e8c90bccf7d53803
aidan melville <amelville@lcs.on.ca>
2019-11-28 14:29:25 -0500

    Initial Commit

---
+++ Snow Calls/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Snow Calls
+//
+//  Created by Melville, Aidan on 2019-11-28.
+//  Copyright © 2019 Melville, Aidan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
